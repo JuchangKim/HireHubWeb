73adcf6c48d065c4418add29279e3cc5a462e519
JuchangKim <132308875+JuchangKim@users.noreply.github.com>
2025-01-12 00:26:50 +1300

    Update seedNews.js

--- server/scripts/seedNews.js
+++ server/scripts/seedNews.js
@@ -4,7 +4,7 @@ const News = require('../models/News');
 
 dotenv.config();
 
-const uri = 'mongodb+srv://Hirehub:1234567Wa%40%23%24@cluster0.hqn5s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; 
+const uri = 'mongodb+srv://brad193026:rla8230643@hirehubweb.pvnmg.mongodb.net/?retryWrites=true&w=majority&appName=HireHubWeb'; 
 mongoose.connect(uri)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
